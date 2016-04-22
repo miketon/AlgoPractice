cc212b73614e68c0954447a0bd6f3549ce92d6e3
Mike Ton <mike.ton@gmail.com>
2016-04-21 22:04:26 -0700

    Linear Search Practice

---
+++ AlgoPractice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AlgoPractice {
+
+  class MainClass {
+    public static void Main (string[] args) {
+      Console.WriteLine ("Hello World!");
+
+      int[] testList = {5, 8, 3, 7, 5, 1, 31, 12, 5};
+
+      Console.WriteLine("Index of value 7 {0}", findIndexOf(testList, 7));
+      Console.WriteLine("Index of value 5 {0}", findIndexOf(testList, 5));
+      Console.WriteLine("Index of value 13511 {0}", findIndexOf(testList, 13511));
+    }
+
+    // Linear Search
+    private static int findIndexOf(int[] array_IN, int targetIndex){
+      for(var i=0; i<array_IN.Length; i++){
+        if(targetIndex == array_IN[i]){
+          return i; //returns the index of the first entry in the array that matches targetIndex
+        }
+      }
+      return -1; // negative number = targetIndex doesn't exist in this array
+    }
+
+  }
+}
