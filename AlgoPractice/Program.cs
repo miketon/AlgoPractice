d11a307b5167677251a814d490208805b995a5f3
Mike Ton <mike.ton@gmail.com>
2016-04-23 23:38:48 -0700

    Breaking down cost of elements via notes.

--- AlgoPractice/Program.cs
+++ AlgoPractice/Program.cs
@@ -25,13 +25,25 @@ namespace AlgoPractice {
     // Invariant has an : init, maintenance, and termination. True for algorithm, one of which is a for loop.
     private static int findIndexOf(int[] array_IN, int targetIndex){
       //  init        termination     maintenance
+      //  cost 1    n+1                 n
+      //      c1    c2                 c3
       for(var i=0; i<array_IN.Length; i++){
         // termination
+        // cost        n (because of < comparison, n+1 if =<)
+        //             c4
         if(targetIndex == array_IN[i]){
+          // cost 0 ???
           return i; //returns the index of the first entry in the array that matches targetIndex
         }
       }
+      // cost 1
       return -1; // negative number = targetIndex doesn't exist in this array
+      // Total = c1 + c2(n+1) + c3n + c4n + 1
+      //         c1 + c2n +c2 + c3n + c4n + 1 // unwind
+      //         c2n + c3n + c4n + c1+ c2 + 1 // group
+      //         n(c2 + c3 + c4) + c1+ c2 + 1 // factor out n
+      //    10,000 -----10------   --100-   1 // value heatmap, n * 10,000 higher order of growth...n is dominant value
+      //    n2 -> n -> log(n)
     }
 
   }
